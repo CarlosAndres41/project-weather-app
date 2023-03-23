81db98dc5c8ca5f8af365dbdadfdc1aa5c1ef792
Carlos Andrés <ca.castaneda@outlook.com>
2023-03-23 00:18:02 -0500

    Display current date and time

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const forecast =
     'http://api.weatherapi.com/v1/forecast.json?key=19f4e2729c45485f99c12545232203&q=London&days=5&aqi=no&alerts=no';
-// Selectors
 
+// Selectors
 const city = document.querySelector('.city');
 const region = document.querySelector('.region');
 const country = document.querySelector('.country');
@@ -9,6 +9,8 @@ const logo = document.querySelector('.logo');
 const condition = document.querySelector('.condition');
 const celcius = document.querySelector('.celcius');
 const farenheit = document.querySelector('.farenheit');
+const time = document.querySelector('.time');
+const date = document.querySelector('.date');
 
 async function getData() {
     try {
@@ -28,3 +30,19 @@ async function getData() {
 }
 
 getData();
+
+//Display real-time
+const interval = setInterval(() => {
+    let currentTime = new Date().toTimeString().slice(0, 8);
+    time.innerHTML = currentTime;
+}, 1000);
+
+// Display date
+var options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+};
+var today = new Date();
+date.innerHTML = today.toLocaleDateString('en-US', options); // Saturday, September 17, 2016
