f27e634e17084655f084eefff4f08adc73e716ff
Carlos Andrés <ca.castaneda@outlook.com>
2023-03-22 23:40:15 -0500

    Display weather data

--- script.js
+++ script.js
@@ -3,17 +3,31 @@ const current =
 const forecast =
     'http://api.weatherapi.com/v1/forecast.json?key=19f4e2729c45485f99c12545232203&q=London&aqi=no';
 
+// Selectors
+
+const city = document.querySelector('.city');
+const region = document.querySelector('.region');
+const country = document.querySelector('.country');
+const logo = document.querySelector('.logo');
+const condition = document.querySelector('.condition');
+const celcius = document.querySelector('.celcius');
+const farenheit = document.querySelector('.farenheit');
+
 async function getData() {
     try {
-        const currentResponse = await fetch(current, { mode: 'cors' });
-        const currentWeatherData = await currentResponse.json();
         const forecastResponse = await fetch(forecast, { mode: 'cors' });
         const forecastWeatherData = await forecastResponse.json();
-        return Promise.all([currentWeatherData, forecastWeatherData]);
+        console.log(forecastWeatherData);
+        city.innerHTML = forecastWeatherData.location.name;
+        region.innerHTML = forecastWeatherData.location.region;
+        country.innerHTML = forecastWeatherData.location.country;
+        logo.src = forecastWeatherData.current.condition.icon;
+        condition.innerHTML = forecastWeatherData.current.condition.text;
+        celcius.innerHTML = `${forecastWeatherData.current['temp_c']}°C`;
+        farenheit.innerHTML = `${forecastWeatherData.current['temp_f']}°F`;
     } catch (error) {
         console.log(error);
     }
 }
 
-const data = getData();
-console.log(data);
+getData();
